d935cd19752f89d5f85ac49dd9b5aeafc53ac732
Shubhankar Panse <SSPanse@bitbucket.org>
2018-12-15 16:23:52 -0500

    updated

--- pixel_RNN_style.py
+++ pixel_RNN_style.py
@@ -6,19 +6,19 @@ from torch.autograd import Variable
 
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-input_size=1024
-hidden_size=1024
-code_size=1024
-batch_size=1
+#input_size=1024
+#hidden_size=1024
+#code_size=1024
+#batch_size=1
 
 class Autoregressive_RNN:
-	def __init__(self,num_layers=1,batch_size=batch_size):
+	def __init__(self,input_size,hidden_size,num_layers=1):
 		super(Autoregressive, self).__init()
-			self.hidden_size=hidden_size
-			self.batch_size=batch_size
-			self.num_layers=num_layers
-			self.rnn=nn.GRU(input_size,hidden_size,num_layers=self.num_layers)
-			self.linear=nn.Linear(hidden_size,code_size)
+		self.hidden_size=hidden_size
+		#self.batch_size=batch_size
+		self.num_layers=num_layers
+		self.rnn=nn.GRU(input_size,hidden_size,num_layers=self.num_layers)
+		self.linear=nn.Linear(hidden_size,code_size)
 
 	def forward(self,X):
 		return self.generate(self.autoregress(X))
