8b799b3cebd52db0a682d72e3b4feb0ccd65af3b
Shubhankar Panse <shubhankarpanse1995@gmail.com>
2018-12-21 23:08:11 -0500

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
-def plot(trains,valids):
-	plt.plot(trains)
-	plt.ylabel("train loss")
-	plt.xlabel("epochs")
-	plt.title("Train Losses")
-	plt.savefig("./Contrastive-Predictive-Coding/train_loss.png",bbox_inches='tight')
-	plt.clf()
-	plt.plot(valids)
-	plt.ylabel("validation loss")
-	plt.xlabel("epochs")
-	plt.title("Validation Losses")
-	plt.savefig("./Contrastive-Predictive-Coding/validation_loss.png",bbox_inches='tight')
+def plot(trains,valids,dataset,code_size):
+        plt.plot(trains)
+        plt.ylabel("train loss")
+        plt.xlabel("epochs")
+        plt.title("Train Losses")
+        plt.savefig("./Contrastive-Predictive-Coding/train_loss_"+dataset+"_"+str(code_size)+".png",bbox_inches='tight')
+        plt.clf()
+        plt.plot(valids)
+        plt.ylabel("validation loss")
+        plt.xlabel("epochs")
+        plt.title("Validation Losses")
+        plt.savefig("./Contrastive-Predictive-Coding/validation_loss_"+dataset+"_"+str(code_size)+".png",bbox_inches='tight')
+
 	
